02b54df9fcfb42139cb1a5e9ea488dbef377eaa0
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2022-04-16 14:52:30 -0700

    Working

--- src/homeKit2.cpp
+++ src/homeKit2.cpp
@@ -5,14 +5,15 @@
 #include "hardware.h"
 #include "logging.h"
 
+#include <math.h>
+
 #include <arduino_homekit_server.h>
 
 homeKit2 homeKit2::instance;
 
 void homeKit2::begin()
 {
-    chaCurrentTemperature.value.float_value = hardware::instance.getTemperatureC();
-    chaHumidity.value.float_value = hardware::instance.getHumidity();
+    updateChaValues();
 
     accessoryName = config::instance.data.hostName;
 
@@ -27,6 +28,24 @@ void homeKit2::begin()
     arduino_homekit_setup(&config);
 
     LOG_INFO(F("HomeKit Server Running"));
+
+    hardware::instance.temperatureChangeCallback.addConfigSaveCallback(std::bind(&homeKit2::notifyTemperatureChange, this));
+    hardware::instance.humidityChangeCallback.addConfigSaveCallback(std::bind(&homeKit2::notifyHumidityChange, this));
+
+    notifyTemperatureChange();
+    notifyHumidityChange();
+}
+
+void homeKit2::notifyTemperatureChange()
+{
+    updateChaValue(chaCurrentTemperature, hardware::instance.getTemperatureC());
+    homekit_characteristic_notify(&chaCurrentTemperature, chaCurrentTemperature.value);
+}
+
+void homeKit2::notifyHumidityChange()
+{
+    updateChaValue(chaHumidity, hardware::instance.getHumidity());
+    homekit_characteristic_notify(&chaHumidity, chaHumidity.value);
 }
 
 void homeKit2::loop()
@@ -49,22 +68,46 @@ void homeKit2::updatePassword(const char *password)
     homeKit2::instance.password = password;
 }
 
+void homeKit2::updateChaValue(homekit_characteristic_t &cha, float value)
+{
+    if (!isnan(value))
+    {
+        cha.value.is_null = false;
+        cha.value.float_value = value;
+    }
+    else
+    {
+        cha.value.is_null = true;
+    }
+}
+
+void homeKit2::updateChaValues()
+{
+    updateChaValue(chaCurrentTemperature, hardware::instance.getTemperatureC());
+    updateChaValue(chaHumidity, hardware::instance.getHumidity());
+}
+
 bool read_storage(uint32 srcAddress, byte *desAddress, uint32 size)
 {
+    LOG_TRACE(F("Reading HomeKit data from ") << srcAddress << F(" size ") << size);
     const auto &data = config::instance.data.homeKitPairData;
-
-    if (data.size() > srcAddress + size)
+    if (data.size() < (srcAddress + size))
     {
-        return false;
+        LOG_TRACE(F("Returning empty for ") << srcAddress << F(" size ") << size);
+        memset(desAddress, 0xFF, size);
     }
-    memcpy(desAddress, data.data() + srcAddress, size);
+    else
+    {
+        memcpy(desAddress, data.data() + srcAddress, size);
+    }
     return true;
 }
 
 bool write_storage(uint32 desAddress, byte *srcAddress, uint32 size)
 {
+    LOG_TRACE(F("Writing HomeKit data at ") << desAddress << F(" size ") << size);
     auto &data = config::instance.data.homeKitPairData;
-    if (desAddress + size < data.size())
+    if ((desAddress + size) > data.size())
     {
         data.resize(desAddress + size);
     }
@@ -76,7 +119,7 @@ bool write_storage(uint32 desAddress, byte *srcAddress, uint32 size)
 
 bool reset_storage()
 {
-    config::instance.data.homeKitPairData.empty();
+    config::instance.data.homeKitPairData.resize(0);
     config::instance.save();
     return true;
 }
