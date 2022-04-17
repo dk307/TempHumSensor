f05073272e406361f54a118bcad1328d3ea0b7cf
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2022-04-16 17:18:09 -0700

    Done

--- lib/EspHap/src/cJSON.c
+++ lib/EspHap/src/cJSON.c
@@ -509,14 +509,14 @@ static cJSON_bool print_number(const cJSON * const item, printbuffer * const out
     }
     else
     {
-        /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-        length = sprintf((char*)number_buffer, "%1.15g", d);
+        /* Try 6 decimal places of precision to avoid nonsignificant nonzero digits */
+        length = sprintf((char*)number_buffer, "%1.6", d);
 
         /* Check whether the original double can be recovered */
         if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
         {
-            /* If not, print with 17 decimal places of precision */
-            length = sprintf((char*)number_buffer, "%1.17g", d);
+            /* If not, print with 8 decimal places of precision */
+            length = sprintf((char*)number_buffer, "%1.8g", d);
         }
     }
 
